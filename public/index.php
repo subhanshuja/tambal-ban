ed1194c70fe2b65de8b1e8a973161f9910d41411
subhanshuja <subhan.nooriansyah@gmail.com>
2020-12-24 09:48:47 +0700

    update references interconnect all code into one file

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
-<!-- call app.php/ main.php for execute all php inside folder app/ -->
+<!-- 
+    call app.php/ main.php for execute all php inside folder app/ 
+    references CodeIgneter4
+    https://github.com/codeigniter4/CodeIgniter4/blob/develop/public/index.php
+-->
 
 <?php
 
